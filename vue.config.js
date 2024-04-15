17425260294f2275f39ce71f64ce6926d40f8583
GradmersQw <gradmersqw@gmail.com>
2024-04-15 06:08:46 +0300

    Updates

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // eslint-disable-next-line prettier/prettier
-  publicPath: process.env.NODE_ENV === "production"
-      ? "/test-task-medods-register/"
-      : "/",
-});
